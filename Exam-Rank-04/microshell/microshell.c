3acb396466f1c6cedd97eb450d1e3e7160fc74cf
Karim Chaouki <kchaouki@c2r9p6.1337.ma>
2023-06-21 13:42:11 +0100

    exam

--- Exam-Rank-04/microshell/microshell.c
+++ Exam-Rank-04/microshell/microshell.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchaouki <kchaouki@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 13:35:12 by kchaouki          #+#    #+#             */
+/*   Updated: 2023/06/21 13:35:13 by kchaouki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -80,3 +92,35 @@ int main(int ac, char **av, char **env)
 	}
 	return (status);
 }
+
+// Assignment name  : microshell
+// Expected files   : microshell.c
+// Allowed functions: malloc, free, write, close, fork, waitpid, signal, kill, exit, chdir, execve, dup, dup2, pipe, strcmp, strncmp
+// --------------------------------------------------------------------------------------
+
+// Write a program that will behave like executing a shell command
+// - The command line to execute will be the arguments of this program
+// - Executable's path will be absolute or relative but your program must not build a path (from the PATH variable for example)
+// - You must implement "|" and ";" like in bash
+// 	- we will never try a "|" immediately followed or preceded by nothing or "|" or ";"
+// - Your program must implement the built-in command cd only with a path as argument (no '-' or without parameters)
+// 	- if cd has the wrong number of argument your program should print in STDERR "error: cd: bad arguments" followed by a '\n'
+// 	- if cd failed your program should print in STDERR "error: cd: cannot change directory to path_to_change" followed by a '\n' with path_to_change replaced by the argument to cd
+// 	- a cd command will never be immediately followed or preceded by a "|"
+// - You don't need to manage any type of wildcards (*, ~ etc...)
+// - You don't need to manage environment variables ($BLA ...)
+// - If a system call, except execve and chdir, returns an error your program should immediatly print "error: fatal" in STDERR followed by a '\n' and the program should exit
+// - If execve failed you should print "error: cannot execute executable_that_failed" in STDERR followed by a '\n' with executable_that_failed replaced with the path of the failed executable (It should be the first argument of execve)
+// - Your program should be able to manage more than hundreds of "|" even if we limit the number of "open files" to less than 30.
+
+// for example this should work:
+// $>./microshell /bin/ls "|" /usr/bin/grep microshell ";" /bin/echo i love my microshell
+// microshell
+// i love my microshell
+// $>
+
+// Hints:
+// Don't forget to pass the environment variable to execve
+
+// Hints:
+// Do not leak file descriptors!
